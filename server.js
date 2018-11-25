d40802885a6e04fffb129571efa6f34349fb8550
Shane Moe <ssoemoe@terpmail.umd.edu>
2018-11-24 20:58:26 -0500

    divide logo added

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const path = require("path");
+const port = 450;
 const app = express();
-const path = require ("path");
-const port = 4500;
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 
-/* To run the Node application, go to CMD, change the directory to the application folder
-and run `node server`*/
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+
+//routers
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + "/View/login.html")));
-app.listen(port, ()=>console.log(`The application is running on localhost:${port}`));
+app.listen(port, ()=>console.log(`The application is running on localhost: ${port}`));
