0ced29ab2c5ce866f81c9a220c57114ff231abb6
Shane Moe <ssoemoe@terpmail.umd.edu>
2018-11-25 21:18:21 -0500

    controller deleted and server js updated

--- server.js
+++ server.js
@@ -6,25 +6,23 @@ const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const db = require("./Model/dbtest");
 const mysql = require("./Model/model");
-mysql.startConnection();
-let conn = mysql.getConnection();
 
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
-//routers
+//functions
 let sendLoginPage = (req, res) => res.sendFile(path.join(__dirname + "/View/login.html"));
+mysql.startConnection();
+let conn = mysql.getConnection();
 
+//routers
 app.get('/', sendLoginPage);
 app.get("/login.html", sendLoginPage);
 app.get("/homepage.html", (req, res) => res.sendFile(path.join(__dirname + "/View/homePage.html")));
 
-//db testing
+//testing
 app.get("/dbtest", (req, res) => {db.getPerson(req, res, conn)});
 
-/*app.put();
-app.delete();
-app.post();*/
 app.listen(port, ()=>console.log(`The application is running on localhost: ${port}`));
