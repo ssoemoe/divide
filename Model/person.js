ffce6e264fc6eecd25554861d27c7b59f19a10d9
Shane Moe <ssoemoe@terpmail.umd.edu>
2018-12-04 00:46:28 -0500

    login fixed

--- Model/person.js
+++ Model/person.js
@@ -14,7 +14,7 @@ module.exports = {
     addPerson: function (req, res, conn, fname, lname, username, email, image, pwd, phNum, position, team) {
         let record = null;
 
-        conn.query(`insert into person(firstName, lastName, username, email, image, password, officePhoneNumber, position, team) ` + 
+        conn.query(`insert into person(firstName, lastName, username, email, image, password, phone, position, team) ` + 
         `values("${fname}", "${lname}", "${username}", "${email}", "${image}` + tools.random(1, 10) +`.png", "${pwd}", "${phNum}", "${position}", "${team}")`,
         (err, result) => {
             if(!err) {
