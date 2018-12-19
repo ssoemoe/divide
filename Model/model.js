c99605184630f38fbabd6fd9e28b7d6c798c47e6
Shane Moe <ssoemoe@terpmail.umd.edu>
2018-12-18 21:12:32 -0500

    root password changed

--- Model/model.js
+++ Model/model.js
@@ -1,8 +1,8 @@
 const mysql = require("mysql");
 const connection = mysql.createConnection({
     host : "localhost",
-    user : "root",
-    password : "",
+    user : "ssoemoe",
+    password : "root",
     database: "divide"
 });
 
